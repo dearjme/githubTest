bd2f78438a885d58c73dcae06b4ff762247f964d
smhrd <smhrd@59.3.122.232>
2021-09-27 16:47:59 +0900

    first commit

--- ex0923_인터페이스/src/인터페이스기초/Myeongeun.java
+++ ex0923_인터페이스/src/인터페이스기초/Myeongeun.java
@@ -1,12 +1,19 @@
 package 인터페이스기초;
 
 public class Myeongeun extends Person implements Codable {
+	
+	// window -> show view -> git repositories
+	
+	// commit push pull
+	// 충돌이 일어나지 않으려면 변경사항이 있다.
+	//  commit -> pull -> push
 
 	@Override
 	public void coding() {
-		// TODO Auto-generated method stub
 		System.out.println("수정수정");
 		
 	}
+	
+	// 어디가셨나요 그대여 
 
 }
